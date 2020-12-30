e889ac58e18ad3802b0b2cfaf4425f825fc939b0
Odell Purifoy <odell@visitdays.com>
2020-12-29 20:08:30 -0500

    Added columns to planet schema

--- app/serializers/planet_serializer.rb
+++ app/serializers/planet_serializer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class PlanetSerializer < ActiveModel::Serializer
-  attributes :id, :name
+  attributes :id, :name, :planet_type, :surface_temp, :surface_type, :core_type
   has_many :moons
 end
